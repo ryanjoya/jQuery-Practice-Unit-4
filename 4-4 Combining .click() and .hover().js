5f08b5a81e6eb1e29f943ff7a97ac53f4bdff85a
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-12-21 23:52:54 -0600

    Create 4-4 Combining .click() and .hover().js

---
+++ 4-4 Combining .click() and .hover().js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+    {
+    $('div').hover(function()
+        {
+        $(this).addClass('red');
+        }).click(function()
+    {
+    $(this).fadeOut('fast');
+    });
+});
